caee5a83cf3e47e7c603599f36b5c6147c3d16b9
xinyuanfang_gw <xyf080218@163.com>
2021-02-25 16:45:52 +0800

    init

--- set-meal/set-meal-core/src/main/java/com/telecom/module/setmeal/core/entity/OptimizeCard.java
+++ set-meal/set-meal-core/src/main/java/com/telecom/module/setmeal/core/entity/OptimizeCard.java
@@ -1,8 +1,18 @@
 package com.telecom.module.setmeal.core.entity;
 
 
+import com.telecom.ecloudframework.base.core.model.GBaseModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-public class OptimizeCard {
+import java.io.Serializable;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class OptimizeCard extends GBaseModel implements Serializable{
     private String id;
 
     private String classifyId;
